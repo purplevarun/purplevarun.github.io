74aeccca37d2734c741f43a275a0766d7546fbdc
purplevarun <purplevarun@gmail.com>
2023-12-31 20:40:19 +0530

    [feat] add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from 'react'
+
 const App = () => {
-    return <h1>Hello world</h1>
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true)
+        }, 1000)
+    })
+
+    if (loaded) {
+        return <h1>Hello world</h1>
+    } else {
+        return <h1>Loading</h1>
+    }
 }
 export default App
